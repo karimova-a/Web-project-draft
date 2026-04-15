6d374b16a302a51defef0305685350edb5901e19
Aigerim Karimova <a_karimova@kbtu.kz>
2026-04-15 15:07:53 +0500

    Delete Desktop directory

--- Desktop/moodflix/backend/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Genre, Movie, WatchlistItem, MoodEntry, UserProfile
-
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(WatchlistItem)
-admin.site.register(MoodEntry)
-admin.site.register(UserProfile)
